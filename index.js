26158b1c641a2dcc1eb4d693c301385e3dfc7fbf
penneytech <99091373+penneytech@users.noreply.github.com>
2024-09-09 09:20:23 -0400

    First commit.

---
+++ index.js
@@ -0,0 +1,12 @@
+/* 
+You are creating an app that keeps track of how old the produce in your fridge is. You will track the age of lettus, apples, and carrots currently inside.
+
+1) Declare a variable for the age of each type of vegetable (do not assign a value for now)
+
+2) Create an IF statement for each type of produce that will log the message "The (vegetable) is/are bad" when the age is greater than:
+    a) 8 days for lettus.
+    b) 15 days for apples.
+    c) 20 days for carrots.
+
+3) Set the initial value of each variable so the messages from step 2 are triggered. 
+*/
